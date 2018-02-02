1c0bb7a3445996208267b0216ae8fca416741471
vladimirrim <vladimirrim98@gmal.com>
2018-02-02 17:04:19 +0300

    Delete unnecessary files.

--- app/app/src/main/java/org/telegram/api/decryptedmessage/TLDecryptedMessageAction.java
+++
@@ -1,9 +0,0 @@
-package org.telegram.api.decryptedmessage;
-
-import org.telegram.tl.TLObject;
-
-/**
- * The type TL decrypted message action.
- */
-public abstract class TLDecryptedMessageAction extends TLObject {
-}
--- app/app/src/main/java/org/telegram/api/upload/file/TLAbsFile.java
+++
@@ -1,9 +0,0 @@
-package org.telegram.api.upload.file;
-
-import org.telegram.tl.TLObject;
-
-/**
- * The type TL file.
- */
-public abstract class TLAbsFile extends TLObject {
-}
