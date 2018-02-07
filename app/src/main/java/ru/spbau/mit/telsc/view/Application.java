e064ebc30d77e3c60d599fa1bfa7b991885da91f
RamSaw <mikhail@pravilov.com>
2018-02-07 13:24:26 +0300

    Corrected issues: 20-24.

--- app/src/main/java/ru/spbau/mit/telsc/view/Application.java
+++ app/src/main/java/ru/spbau/mit/telsc/view/Application.java
@@ -10,21 +10,6 @@ import android.widget.Toast;
 import ly.img.android.PESDK;
 
 public class Application extends android.app.Application {
-    @Nullable
-    public static String getRealPathFromURI(Context context, Uri contentUri) {
-        String[] data = {MediaStore.Images.Media.DATA};
-        try (Cursor cursor = context.getContentResolver().query(contentUri, data, null, null, null)) {
-            if (cursor != null) {
-                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
-                return cursor.getString(column_index);
-            } else {
-                Toast.makeText(PESDK.getAppContext(), "Cannot get real path from URI\n", Toast.LENGTH_LONG).show();
-            }
-        }
-        return null;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
